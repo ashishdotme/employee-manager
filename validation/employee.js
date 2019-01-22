d7f81483b50160b654e6d4e9f25fbeb167559ff6
Ashish Patel <ashishsushilpatel@gmail.com>
2019-01-22 12:24:21 +0530

    Add validations

--- validation/employee.js
+++ validation/employee.js
@@ -8,12 +8,28 @@ module.exports = function validateEmployeeInput(data) {
   data.mobile = !isEmpty(data.mobile) ? data.mobile : "";
   data.email = !isEmpty(data.email) ? data.email : "";
 
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters";
+  }
+
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
 
+  if (!Validator.isNumeric(data.mobile)) {
+    errors.mobile = "Mobile number can only have numbers";
+  }
+
+  if (!Validator.isLength(data.mobile, { min: 10, max: 10 })) {
+    errors.mobile = "Mobile number must have 10 digits";
+  }
+
   if (Validator.isEmpty(data.mobile)) {
     errors.mobile = "Mobile field is required";
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
   }
 
   if (Validator.isEmpty(data.email)) {
